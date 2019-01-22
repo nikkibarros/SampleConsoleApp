d8a1910b72407fcead5109221eb836109e170ead
Allan Tolentino <allanto@magenic.com>
2019-01-22 15:53:51 +0800

    Add new file

---
+++ Foo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SampleConsoleApp
+{
+    class Foo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
